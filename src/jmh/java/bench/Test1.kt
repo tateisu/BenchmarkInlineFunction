8cfbe9e26ab17935d84c708dd4def7a974b84ecd
tateisu <tateisu@gmail.com>
2019-12-31 20:34:21 +0900

    compare Holder implements simple assignment or real HashMap.

--- src/jmh/java/bench/Test1.kt
+++ src/jmh/java/bench/Test1.kt
@@ -3,7 +3,7 @@ package bench
 import org.openjdk.jmh.annotations.*
 
 // Something like HashMap<String,String>
-class Holder {
+class Assign_Holder {
     @Volatile private var value :Any = ""
 
     operator fun set(
@@ -13,21 +13,40 @@ class Holder {
     }
 }
 
-class PrefKey(private val key: String) {
-    fun put(holder: Holder, value: String) {
+class Assign_PrefKey(private val key: String) {
+    fun put(holder: Assign_Holder, value: String) {
         holder[key] = value
     }
 }
 
-fun Holder.putA(pref: PrefKey, value: String): Holder {
+fun Assign_Holder.putA(pref: Assign_PrefKey, value: String): Assign_Holder {
     pref.put(this, value)
     return this
 }
 
-fun Holder.putB(pref: PrefKey, value: String): Holder =
+fun Assign_Holder.putB(pref: Assign_PrefKey, value: String): Assign_Holder =
     apply {
         pref.put(this, value)
     }
+
+class HashMap_Holder : HashMap<String,String>()
+
+class HashMap_PrefKey(private val key: String) {
+    fun put(holder: HashMap_Holder, value: String) {
+        holder[key] = value
+    }
+}
+
+fun HashMap_Holder.putA(pref: HashMap_PrefKey, value: String): HashMap_Holder {
+    pref.put(this, value)
+    return this
+}
+
+fun HashMap_Holder.putB(pref: HashMap_PrefKey, value: String): HashMap_Holder =
+    apply {
+        pref.put(this, value)
+    }
+
 
 // Benchmark classes should not be final. // [jmh.bench.Test1]
 
@@ -39,16 +58,30 @@ fun Holder.putB(pref: PrefKey, value: String): Holder =
 @Suppress("unused")
 open class Test1 {
 
-    private var holder= Holder()
-    private var pref= PrefKey("foo")
+    private var Assign_holder= Assign_Holder()
+    private var Assign_pref= Assign_PrefKey("foo")
+
+    private var HashMap_holder= HashMap_Holder()
+    private var HashMap_pref= HashMap_PrefKey("foo")
 
     @Benchmark
-    fun usePutA(){
-        holder.putA(pref, "zap")
+    fun Assign_usePutA(){
+        Assign_holder.putA(Assign_pref, "zap")
     }
 
     @Benchmark
-    fun usePutB(){
-        holder.putB(pref, "zap")
+    fun Assign_usePutB(){
+        Assign_holder.putB(Assign_pref, "zap")
     }
+
+    @Benchmark
+    fun HashMap_usePutA(){
+        HashMap_holder.putA(HashMap_pref, "zap")
+    }
+
+    @Benchmark
+    fun HashMap_usePutB(){
+        HashMap_holder.putB(HashMap_pref, "zap")
+    }
+
 }
